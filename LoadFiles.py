656c337e63e695c4bdecc1f597037c19c23106d6
JonathanREB <43258639+JonathanREB@users.noreply.github.com>
2019-01-14 20:48:39 -0600

    Add files via upload

--- LoadFiles.py
+++ LoadFiles.py
@@ -7,7 +7,7 @@ df_keys = ["Order Date","Cust No", "Postcode", "Agent", "Order ref", "Order Sour
             "P&P net", "Invoice Fee", "Total net", "Item Type", "Ship Date", "State", 
             "Delivery Date", "Carrier", "Tracking No"]
 
-def load_files():
+def get_dataframe(force):
     cur_dir = os.getcwd()
     #print(cur_dir)
     
@@ -15,27 +15,35 @@ def load_files():
     df_cur   = pd.DataFrame()
     df_acc   = pd.DataFrame()
     
-    if 'input' not in cur_dir:
-        os.chdir('input')
-        cur_dir = os.getcwd()
-        print(f'Change to <{cur_dir}> directory')
+    if force == True:
+        if 'input' not in cur_dir:
+            os.chdir('input')
+            cur_dir = os.getcwd()
+            print(f'Change to <{cur_dir}> directory')
 
-    print(f'Already into <{cur_dir}> directory')
-    files_list = [str(f) for f in glob.glob("*.csv")]
-    for file_str in files_list:
-        print(f'Loading file <{file_str}>...')
-        df_cur = pd.read_csv(file_str,skiprows=4, encoding='latin-1')
-        df_cur = df_cur[df_keys]
-        df_acc = df_acc.append(df_cur)
-        #print(df_acc['Order Date'].count())
+        #print(f'Already into <{cur_dir}> directory')
+        files_list = [str(f) for f in glob.glob("*.csv")]
+        for file_str in files_list:
+            print(f'Loading file <{file_str}>...')
+            df_cur = pd.read_csv(file_str,skiprows=4, encoding='latin-1')
+            df_cur = df_cur[df_keys]
+            df_acc = df_acc.append(df_cur)            
+            #print(df_acc['Order Date'].count())
+            #print(df_total['Order Date'].count())
+
+        #df_total = df_acc    
         #print(df_total['Order Date'].count())
-
-    #df_total = df_acc    
-    #print(df_total['Order Date'].count())
-    os.chdir('../')    
+        print('Datafram generated')
+        os.chdir('../')  
+        df_acc.to_csv("output/main_dataframe.csv", encoding='utf-8', index=False)
+    else:
+        print(f'Loading from file <output/main_dataframe.csv>...')
+        try:
+            df_acc = pd.read_csv("output/main_dataframe.csv", encoding='latin-1')
+        except :
+            print('Error: file <output/main_dataframe.csv> not found')
+        print('Dataframe generated')
+        
     return df_acc                 
                       
-#load_files()            
-            
-#files = []
-#files = os.listdir()
+#get_dataframe(False)            
